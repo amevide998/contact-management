86146e989f579be8f5f6f361ae7adcb5285fefba
amevide998 <hdsvidi@gmail.com>
2023-10-07 19:36:36 +0700

    initial User repository

---
+++ src/main/java/hdscode/springrestfull/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package hdscode.springrestfull.repository;
+
+import hdscode.springrestfull.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, String> {
+}
