22fb1cff80c59e81cb048f5cc74f30ea533e1b1b
amevide998 <hdsvidi@gmail.com>
2023-10-07 19:33:26 +0700

    adding Address Entity

---
+++ src/main/java/hdscode/springrestfull/entity/Address.java
@@ -0,0 +1,31 @@
+package hdscode.springrestfull.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="addresses")
+public class Address {
+    @Id
+    private String id;
+
+    private String street;
+
+    private String city;
+
+    private String country;
+
+    @Column(name="postal_code")
+    private String postalCode;
+
+    @ManyToOne
+    @JoinColumn(name = "contact_id", referencedColumnName = "id")
+    private Contact contact;
+}
