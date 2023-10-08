42e1b8797f25026839cce8c3105b2bfc16e9b06e
amevide998 <hdsvidi@gmail.com>
2023-10-08 13:36:18 +0700

    initial user response model

---
+++ src/main/java/hdscode/springrestfull/model/UserResponse.java
@@ -0,0 +1,16 @@
+package hdscode.springrestfull.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UserResponse {
+
+    private String username;
+    private String name;
+}
