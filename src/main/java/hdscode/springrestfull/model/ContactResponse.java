d188351347a7154298a1093ce3c094ee890a0ea8
amevide998 <hdsvidi@gmail.com>
2023-10-08 14:46:30 +0700

    initial ContactResponse Model

---
+++ src/main/java/hdscode/springrestfull/model/ContactResponse.java
@@ -0,0 +1,25 @@
+package hdscode.springrestfull.model;
+
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ContactResponse {
+
+    private String id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String email;
+
+    private String phone;
+
+}
