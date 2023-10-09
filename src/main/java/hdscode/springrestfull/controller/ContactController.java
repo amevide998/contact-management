446c6b9b2c84c69c59d6c339ae48ee27e0fa86b8
amevide998 <hdsvidi@gmail.com>
2023-10-09 08:37:00 +0700

    adding end point get, update, delete, and search

--- src/main/java/hdscode/springrestfull/controller/ContactController.java
+++ src/main/java/hdscode/springrestfull/controller/ContactController.java
@@ -1,15 +1,14 @@
 package hdscode.springrestfull.controller;
 
 import hdscode.springrestfull.entity.User;
-import hdscode.springrestfull.model.ContactResponse;
-import hdscode.springrestfull.model.CreateContactRequest;
-import hdscode.springrestfull.model.WebResponse;
+import hdscode.springrestfull.model.*;
 import hdscode.springrestfull.service.ContactService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class ContactController {
@@ -21,6 +20,56 @@ public class ContactController {
     public WebResponse<ContactResponse> create(User user,@RequestBody CreateContactRequest request){
         ContactResponse response = contactService.create(user, request);
         return WebResponse.<ContactResponse>builder().data(response).build();
+    }
+
+    @GetMapping(path = "/api/contacts/{id}", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public WebResponse<ContactResponse> get(User user, @PathVariable String id){
+        ContactResponse response = contactService.get(user, id);
+        return WebResponse.<ContactResponse>builder().data(response).build();
+    }
+
+    @PutMapping(path = "/api/contacts/{id}",produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public WebResponse<ContactResponse> update(User user, @PathVariable String id, @RequestBody UpdateContactRequest request){
+        request.setId(id);
+        ContactResponse response = contactService.update(user, request);
+        return WebResponse.<ContactResponse>builder().data(response).build();
+
+    }
+
+    @DeleteMapping(path = "/api/contacts/{id}", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public WebResponse<String> delete(User user, @PathVariable String id){
+        contactService.delete(user, id);
+        return WebResponse.<String>builder().data("Ok").build();
+    }
+
+    @GetMapping(path = "/api/contacts",
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public WebResponse<List<ContactResponse>> search(User user,
+                                                     @RequestParam(value = "name", required = false) String name,
+                                                     @RequestParam(value = "email", required = false) String email,
+                                                     @RequestParam(value = "phone", required = false) String phone,
+                                                     @RequestParam(value = "page", required = false, defaultValue = "0") Integer page,
+                                                     @RequestParam(value = "size", required = false, defaultValue = "10") Integer size
+    ){
+        SearchContactRequest searchContactRequest = SearchContactRequest.builder()
+                .name(name)
+                .email(email)
+                .phone(phone)
+                .page(page)
+                .size(size)
+                .build();
+
+        Page<ContactResponse> contactResponse = contactService.search(user, searchContactRequest);
+        return WebResponse.<List<ContactResponse>>builder()
+                .data(contactResponse.getContent())
+                .pagingResponse(
+                        PagingResponse.builder()
+                                .currentPage(contactResponse.getNumber())
+                                .totalPages(contactResponse.getTotalPages())
+                                .size(contactResponse.getSize())
+                                .build())
+                .build();
+
     }
 
 }
