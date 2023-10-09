07d7d87b7b0ddc408350e07d03bd57655ac94a18
amevide998 <hdsvidi@gmail.com>
2023-10-09 08:34:38 +0700

    initial paging response

---
+++ src/main/java/hdscode/springrestfull/model/PagingResponse.java
@@ -0,0 +1,19 @@
+package hdscode.springrestfull.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class PagingResponse {
+
+    private Integer currentPage;
+
+    private Integer totalPages;
+
+    private Integer size;
+}
