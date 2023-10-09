88963fc93dc005bb4c384d649a30b69fb3aa3611
amevide998 <hdsvidi@gmail.com>
2023-10-09 09:42:24 +0700

    adding update & delete service

--- src/main/java/hdscode/springrestfull/service/AddressService.java
+++ src/main/java/hdscode/springrestfull/service/AddressService.java
@@ -5,17 +5,23 @@ import hdscode.springrestfull.entity.Contact;
 import hdscode.springrestfull.entity.User;
 import hdscode.springrestfull.model.AddressResponse;
 import hdscode.springrestfull.model.CreateAddressRequest;
+import hdscode.springrestfull.model.UpdateAddressRequest;
 import hdscode.springrestfull.repository.AddressRepository;
 import hdscode.springrestfull.repository.ContactRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
+@Slf4j
 public class AddressService {
 
     @Autowired
@@ -70,5 +76,61 @@ public class AddressService {
                 .orElseThrow(()-> new ResponseStatusException(HttpStatus.NOT_FOUND, "address not found"));
 
         return mapToAddressResponse(address);
+    }
+
+    @Transactional
+    public AddressResponse update(User user, UpdateAddressRequest request){
+        log.info("address service - update : request = {}", request.getCity());
+
+
+        validationService.validate(request);
+        Contact contact = contactRepository.findFirstByUserAndId(user, request.getContactId())
+                .orElseThrow(()-> new ResponseStatusException(HttpStatus.NOT_FOUND, "contact not found"));
+
+        Address address = addressRepository.findFirstByContactAndId(contact, request.getAddressId())
+                .orElseThrow(()-> new ResponseStatusException(HttpStatus.NOT_FOUND, "address not found"));
+
+        log.info("address service - update : address = {}", address.getCity());
+
+
+        if(Objects.nonNull(request.getCity())){
+            address.setCity(request.getCity());
+        }
+        if(Objects.nonNull(request.getStreet())){
+            address.setStreet(request.getStreet());
+        }
+
+        address.setCountry(request.getCountry());
+
+        if(Objects.nonNull(request.getPostalCode())){
+            address.setPostalCode(request.getPostalCode());
+        }
+        if(Objects.nonNull(request.getProvince())){
+            address.setProvince(request.getProvince());
+        }
+        addressRepository.save(address);
+
+        return mapToAddressResponse(address);
+    }
+
+
+    @Transactional
+    public void remove(User user, String contactId, String addressId){
+        Contact contact = contactRepository.findFirstByUserAndId(user, contactId)
+                .orElseThrow(()-> new ResponseStatusException(HttpStatus.NOT_FOUND, "contact not found"));
+
+        Address address = addressRepository.findFirstByContactAndId(contact, addressId)
+                .orElseThrow(()-> new ResponseStatusException(HttpStatus.NOT_FOUND, "address not found"));
+
+        addressRepository.delete(address);
+    }
+
+    @Transactional
+    public List<AddressResponse> list(User user, String contactId){
+        Contact contact = contactRepository.findFirstByUserAndId(user, contactId)
+                .orElseThrow(()-> new ResponseStatusException(HttpStatus.NOT_FOUND, "contact not found"));
+
+        List<Address> addresses =  addressRepository.findAllByContact(contact);
+        return addresses.stream().map(this::mapToAddressResponse).toList();
     }
 }
