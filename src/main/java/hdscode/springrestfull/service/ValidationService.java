c8a3348e812a5e1f423dbf4bb0cc5b17d88bb87b
amevide998 <hdsvidi@gmail.com>
2023-10-09 18:04:23 +0700

    fixing response for error handling

--- src/main/java/hdscode/springrestfull/service/ValidationService.java
+++ src/main/java/hdscode/springrestfull/service/ValidationService.java
@@ -5,7 +5,9 @@ import jakarta.validation.ConstraintViolation;
 import jakarta.validation.ConstraintViolationException;
 import jakarta.validation.Validator;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.Set;
 
@@ -18,7 +20,7 @@ public class ValidationService {
     public void validate(Object request){
         Set<ConstraintViolation<Object>> constraintViolations = validator.validate(request);
         if(constraintViolations.size() != 0){
-            throw new ConstraintViolationException(constraintViolations);
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, constraintViolations.iterator().next().getPropertyPath().toString() + ": " + constraintViolations.iterator().next().getMessage());
         }
     }
 }
